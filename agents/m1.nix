5588ffaf19a5fd3aa81deabb5d4d5366780124aa
Domen Kožar <domen@dev.si>
2022-12-30 16:58:25 +0000

    allow substituters for hetzner

--- agents/m1.nix
+++ agents/m1.nix
@@ -9,6 +9,7 @@
   nix.extraOptions = ''
     experimental-features = flakes nix-command
   '';
+  nix.settings.trusted-users = ["root" "hetzner"];
 
   networking.hostName = "unfree-m1";
   services.cachix-agent.enable = true;
